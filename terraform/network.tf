e8ead65343647d9247ba6f49e72b58edbee6587b
fosterm-mw <michael.foster@mavenwave.com>
2023-01-20 16:36:48 -0600

    point to mw infra registry

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,6 +1,7 @@
 
 module "ignite-network" {
-  source = "app.terraform.io/fosterm-mw/ignite-network/aws"
+  source = "infra.mavenwave.net/infra-mavenwave/ignite-network/aws"
   vpc_name = "rke2-network"
 }
+
 
