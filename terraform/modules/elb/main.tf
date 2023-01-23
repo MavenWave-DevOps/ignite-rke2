6c9a24e0450fae5cae05a4f3fc880df6b1c4ed2f
fosterm-mw <michael.foster@mavenwave.com>
2023-01-23 09:33:36 -0600

    need in same module

--- terraform/modules/elb/main.tf
+++ terraform/modules/elb/main.tf
@@ -1,4 +1,12 @@
 
+resource "aws_security_group" "controlplane" {
+  name        = var.controlplane_name
+  description = "${var.controlplane_name} security group"
+  vpc_id      = var.vpc_id
+
+  tags = merge({}, var.tags)
+}
+
 resource "aws_security_group_rule" "apiserver" {
   from_port         = var.cp_port
   to_port           = var.cp_port
--- terraform/modules/elb/sg.tf
+++
@@ -1,9 +0,0 @@
-
-resource "aws_security_group" "controlplane" {
-  name        = var.controlplane_name
-  description = "${var.controlplane_name} security group"
-  vpc_id      = var.vpc_id
-
-  tags = merge({}, var.tags)
-}
-
