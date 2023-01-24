39a897e5b8466ec7e05756a845fd9e13af11f412
fosterm-mw <michael.foster@mavenwave.com>
2023-01-24 17:21:44 -0600

    output kubeconfig

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,3 +3,18 @@ output "kubeconfig_path" {
   value = "s3://${module.statestore.bucket}/rke2.yaml"
 }
 
+output "rke2" {
+  value = module.rke2
+}
+
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command = "aws s3 cp ${module.rke2.kubeconfig_path} rke2.yaml"
+  }
+
+  depends_on = [
+    module.rke2
+  ]
+}
+
