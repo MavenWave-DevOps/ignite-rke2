e7350316a2f76e79bf3c50e389983b209482720d
fosterm-mw <michael.foster@mavenwave.com>
2023-01-24 17:22:17 -0600

    kubeconfig test

--- terraform/nodepool.tf
+++ terraform/nodepool.tf
@@ -77,19 +77,19 @@ module "rke2" {
 #   
 #   tags = local.tags
 # }
-
-output "rke2" {
-  value = module.rke2
-}
-
-resource "null_resource" "kubeconfig" {
-  provisioner "local-exec" {
-    interpreter = ["bash", "-c"]
-    command = "aws s3 cp ${module.rke2.kubeconfig_path} rke2.yaml"
-  }
-
-  depends_on = [
-    module.rke2
-  ]
-}
-
+#
+# output "rke2" {
+#   value = module.rke2
+# }
+#
+# resource "null_resource" "kubeconfig" {
+#   provisioner "local-exec" {
+#     interpreter = ["bash", "-c"]
+#     command = "aws s3 cp ${module.rke2.kubeconfig_path} rke2.yaml"
+#   }
+#
+#   depends_on = [
+#     module.rke2
+#   ]
+# }
+#
