9a3a433eb1b5d4a79a63d6a52fa4a46fb305304a
fosterm-mw <michael.foster@mavenwave.com>
2023-01-25 12:03:06 -0600

    this should exist now, might need another layer of bucket

--- terraform/kubeconfig.tf
+++ terraform/kubeconfig.tf
@@ -7,7 +7,7 @@
 resource "null_resource" "kubeconfig" {
   provisioner "local-exec" {
     interpreter = ["bash", "-c"]
-    command = "aws s3 cp s3://ignite-rke2-k4j-rke2/rke2.yaml rke2.yml"
+    command = "aws s3 cp s3://ignite-rke2-k4j-rke2/rke2.yml rke2.yml"
   }
 
   depends_on = [
