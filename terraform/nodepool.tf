01b17e4daec8f61fa65ff29b6418cdf0e8403d5b
fosterm-mw <michael.foster@mavenwave.com>
2023-01-25 10:36:17 -0600

    adding kubeconfig for auth

--- terraform/nodepool.tf
+++ terraform/nodepool.tf
@@ -83,14 +83,14 @@ module "rke2" {
 #   value = module.rke2
 # }
 #
-# resource "null_resource" "kubeconfig" {
-#   provisioner "local-exec" {
-#     interpreter = ["bash", "-c"]
-#     command = "aws s3 cp ${module.rke2.kubeconfig_path} rke2.yaml"
-#   }
-#
-#   depends_on = [
-#     module.rke2
-#   ]
-# }
-#
+resource "null_resource" "kubeconfig" {
+  provisioner "local-exec" {
+    interpreter = ["bash", "-c"]
+    command = "aws s3 cp ${module.rke2.kubeconfig_path} rke2.yaml"
+  }
+
+  depends_on = [
+    module.rke2
+  ]
+}
+
